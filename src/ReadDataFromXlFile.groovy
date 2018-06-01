bc820ce8b554f2cb919865cd3df5aeae9275ddb7
Your Name <ananthsrg@gmail.com>
2018-06-01 14:44:52 +0530

    ananth commit

---
+++ src/ReadDataFromXlFile.groovy
@@ -0,0 +1,8 @@
+
+class ReadDataFromXlFile {
+public static void main(String []args)
+{
+	def f=new File("./data/test.xlsx");
+	def wb;
+}
+}
---
+++ src/StrConCat.groovy
@@ -0,0 +1,13 @@
+
+class StrConCat {
+
+	static main(args) {
+		def s="Old string"
+		println "new string"+s;
+		println 'new string '+s
+		println "new string +s";
+		println 'new string $s';
+		println "new string $s";
+	}
+
+}
